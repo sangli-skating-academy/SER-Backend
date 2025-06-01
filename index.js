5871090dda485030eedd789ee068eb0bdb5a1aa0
Rakesh <rakeshyadavry087@gmail.com>
2025-06-02 02:13:59 +0530

    add vercel request

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://ser-frontend-livid.vercel.app/",
+  "https://ser-frontend-fasb8pxt4-sangli-skating-academy-s-projects.vercel.app",
 ];
 app.use(
   cors({
