19691b761f494190651bcb87366e2f2291e018c8
Rakesh <rakeshyadavry087@gmail.com>
2025-12-28 02:44:51 +0530

    cors

--- config/config.js
+++ config/config.js
@@ -55,8 +55,10 @@ export const CORS_CONFIG = {
     ? process.env.CORS_ORIGINS.split(",").map((origin) => origin.trim())
     : [
         "http://localhost:5173",
-        "www.sangliskating.com",
+        "http://ser-frontend-livid.vercel.app/",
         "https://sangliskating.com",
+        "https://www.sangliskating.com/",
+        "",
       ],
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
